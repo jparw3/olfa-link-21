54c4bd7089fa952f83b699a93e49fec931d0ebbe
Jack <89160981+Jparw@users.noreply.github.com>
2021-11-19 09:31:55 +0000

    few minor changes

--- src/Components/UserProfile/ErrorPage.js
+++ src/Components/UserProfile/ErrorPage.js
@@ -7,7 +7,7 @@ function ErrorPage() {
         <img src='/olfalogo.png' alt="image" style={{ width: '150px' }}/>
         <h1>Profile not found.</h1>
         <h1>If you are a new user, please consider registering at Olfa.</h1>
-        <h2>Read the documendation <a href="https://github.com/jparw/olfa-link-21#readme" target="_blank" rel="noreferrer">here</a>.</h2>
+        <h2>Read the documtendation <a href="https://github.com/jparw/olfa-link-21#readme" target="_blank" rel="noreferrer">here</a>.</h2>
       </div>
     </div>
   )
